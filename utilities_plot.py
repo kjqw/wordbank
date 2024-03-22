ab28f4df8f03ca926e79d034466fc888af9bc0f6
kjqw <johnson0@keio.jp>
2024-03-22 16:37:09 +0900

    update

--- utilities_plot.py
+++ utilities_plot.py
@@ -2,6 +2,7 @@ import pickle
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 import torch
 from matplotlib.axes import Axes
 from matplotlib.figure import Figure
@@ -156,3 +157,98 @@ def plot_arrow(model: VAE, data_ids: list[int], ax: Axes) -> None:
 def child_id_to_data(child_id: int) -> list[int]:
     data_ids = [i[0] for i in child_id_dict[child_id]]
     return data[data_ids]
+
+
+def get_symmetric_points(P1: np.ndarray, P2: np.ndarray, Q: np.ndarray) -> np.ndarray:
+    """
+    与えられた2点P1, P2を通る直線に対して、点Qの対称点を計算する。
+
+    Parameters:
+    P1 (np.ndarray): 直線を定義する最初の点。
+    P2 (np.ndarray): 直線を定義する2番目の点。
+    Q (np.ndarray): 対称点を見つけたい点。
+
+    Returns:
+    np.ndarray: 点Qの対称点
+    """
+    # P1とP2を結ぶベクトルを計算
+    P1P2_vector = P2 - P1
+    # P1からQへのベクトルを計算
+    P1Q_vector = Q - P1
+
+    # P1QベクトルがP1P2ベクトルに投影されたときのスカラーを計算
+    projection_scalar = np.dot(P1Q_vector, P1P2_vector) / np.dot(
+        P1P2_vector, P1P2_vector
+    )
+    # Qの対称点を求めるためのベクトルを計算
+    symmetric_vector = projection_scalar * P1P2_vector - P1Q_vector
+
+    # Qの直線に対する対称点を計算
+    symmetric_point = Q + symmetric_vector * 2
+
+    return symmetric_point
+
+
+def get_vocabulary_dataframe_from_points(
+    model: VAE, points: np.ndarray, points_name: list[str]
+) -> pd.DataFrame:
+    """
+    与えられた潜在空間の座標を語彙のDataFrameに変換する。
+
+    Parameters:
+    points (np.ndarray): 座標のリスト。
+
+    Returns:
+    pd.DataFrame: 語彙のDataFrame。
+    """
+    xs = z_to_x(model, points)
+    tmp1 = []
+    tmp2 = []
+    for key, val in category_dict.items():
+        for id, word in val:
+            tmp1.append(key)
+            tmp2.append(word)
+    df_vocabulary = pd.DataFrame({"category": tmp1, "word": tmp2})
+    for i in range(xs.shape[0]):
+        df_vocabulary[points_name[i]] = xs[i, :]
+    return df_vocabulary
+
+
+def plot_vocabulary_expectations(
+    model: VAE, point: np.ndarray, fig1: Figure, ax1: Axes, fig2: Figure, ax2: Axes
+) -> None:
+    df_vocabulary = get_vocabulary_dataframe_from_points(model, point, ["point"])
+
+    ax1.set_xticks([i for i in range(22)])
+    ax1.set_xticklabels([chr(65 + i) for i in range(22)])
+    ax1.set_ylim(-0.1, 1.1)
+    for i, v in enumerate(ax1.get_xticklabels()):
+        v.set_color(f"C{i}")
+
+    ax1.set_ylabel("Expectation")
+    for i, v in enumerate(df_vocabulary.groupby("category", sort=False)):
+        ax1.scatter([i] * len(v[1]), v[1]["point"], color="C" + str(i), s=10)
+
+    plot_x(model, data, ax2)
+    ax2.scatter(point[:, 0], point[:, 1], color="tab:orange", label="point")
+    ax2.legend()
+
+
+def plot_vocabulary_differences_with_points(
+    df_vocabulary: pd.DataFrame,
+    point_name1: str,
+    point_name2: str,
+    fig: Figure,
+    ax: Axes,
+) -> None:
+    df_diff = df_vocabulary[["category", "word"]].copy()
+    df_diff["diff"] = df_vocabulary[point_name1] - df_vocabulary[point_name2]
+    diff_min = df_diff["diff"].min()
+
+    ax.axhline(y=0, color="grey", linestyle="--")
+    ax.set_xticks([])
+    ax.set_xticklabels([])
+    ax.set_ylabel(f"Difference {point_name1} - {point_name2}")
+    for i, v in enumerate(df_diff.groupby("category", sort=False)):
+        ax.scatter([i] * len(v[1]), v[1]["diff"], color="C" + str(i), s=10)
+        ax.text(i, diff_min, chr(65 + i), ha="center", va="center", color="C" + str(i))
